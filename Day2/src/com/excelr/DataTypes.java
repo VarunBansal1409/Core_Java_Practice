30e1bb0f33fff93ed3a04ed83d52e4e6ba58c026
Varun <varunbansal1409@gmail.com>
2025-11-30 19:23:48 +0530

    Add files via upload

---
+++ Day2/src/com/excelr/DataTypes.java
@@ -0,0 +1,41 @@
+package com.excelr;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		
+		//byte
+		System.out.println(Byte.SIZE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		System.out.println("=============");
+		//short
+		System.out.println(Short.SIZE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		
+		System.out.println("=============");
+		//int
+		System.out.println(Integer.SIZE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		
+		System.out.println("=============");
+		//long
+		System.out.println(Long.SIZE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		
+		System.out.println("=============");
+		//float
+		System.out.println(Float.SIZE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		
+		System.out.println("=============");
+		//double
+		System.out.println(Double.SIZE);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+	}
+}
---
+++ Day2/src/com/excelr/Demo.java
@@ -0,0 +1,14 @@
+package com.excelr;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("java");
+		System.out.println();
+		
+		System.out.print("hello world ");
+		System.out.print("java");
+//		System.out.print();
+	}
+}
